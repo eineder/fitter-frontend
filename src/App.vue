780380471927ffe81874fa7d3c376e0dbec59c81
max <max.eineder@googlemail.com>
2023-09-11 13:53:44 +0200

    Add Amplify components for authentication

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
   <div id="app">
-    <i class="fa-solid fa-thumbs-up fa-5x"></i>
+    <amplify-authenticator usernameAlias="email">
+      <amplify-sign-up slot="sign-up" :form-fields.prop="signUpFields">
+      </amplify-sign-up>
+      <div>
+        <h1 v-if="user && user.attributes">Hey, {{ user.attributes.name }}!</h1>
+        <amplify-sign-out></amplify-sign-out>
+      </div>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
-
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      user: {},
+      signUpFields: [
+        { type: 'username', label: 'Username (Email) *', required: true },
+        { type: 'Password', label: 'Password *', required: true },
+        { type: 'name', label: 'Name *', required: true },
+        { type: 'phone_number', label: 'Phone number *', required: true }
+      ]
+    }
+  },
+  created() {
+    onAuthUIStateChange((state, user) => {
+      if (state === AuthState.SignedIn) {
+        this.user = user;
+        console.log(user);
+      }
+    }
+
+    )
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+
+Amplify.configure({
+  Auth: {
+    region: 'eu-west-1',
+    userPoolId: 'eu-west-1_HPTxEJaFh',
+    userPoolWebClientId: '2n86c01m2bf72grl2b5gsqt2u1',
+    mandatorySignIn: true
+  }
+})
+
+
+const appConfig = {
+  'aws_appsync_graphqlEndpoint': 'https://b47neuwodzfljkjaursz6sedue.appsync-api.eu-west-1.amazonaws.com/graphql',
+  'aws_appsync_region': 'eu-west-1',
+  'aws_appsync_authenticationType': 'AMAZON_COGNITO_USER_POOLS',
+}
+Amplify.configure(appConfig)
 
 Vue.config.productionTip = false
 
