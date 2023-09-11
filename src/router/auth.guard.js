d3ab0fb5fd72ac07e48c68545044fcdbef3bd027
max <max.eineder@googlemail.com>
2023-09-11 14:44:47 +0200

    Add basic views and authentication guard

---
+++ src/router/auth.guard.js
@@ -0,0 +1,13 @@
+import { Auth } from 'aws-amplify'
+
+export default async (to, from, next) => {
+    const isProtected = to.matched.some(route => route.meta.protected)
+    const loggedIn = await Auth.currentUserInfo();
+
+    if (isProtected && !loggedIn) {
+        next("/")
+        return
+    }
+
+    next()
+}
