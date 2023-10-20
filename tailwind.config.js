7c4aaa4da5410d7b5f6a726ed443edad055b3987
max <max.eineder@googlemail.com>
2023-10-20 11:40:58 +0200

    Style RootView

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const defaultTheme = require('tailwindcss/defaultTheme')
 
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true
+    },
+    maxHeight: {
+      'full': '85%',
+    },
+    extend: {
+      fontFamily: {
+        sans: [...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'blue': '#1DA1F2',
+        'darkblue': '#2795D9',
+        'lightblue': '#EFF9FF',
+        'dark': '#657786',
+        'light': '#AAB8C2',
+        'lighter': '#E1E8ED',
+        'lightest': '#F5F8FA',
+      }
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: []
+  }
+}
