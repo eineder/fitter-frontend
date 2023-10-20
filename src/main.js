ceeb3958cfd5e682704e7166b3a89c120f7b658e
max <max.eineder@googlemail.com>
2023-10-20 11:40:26 +0200

    Name .env variables properly

--- src/main.js
+++ src/main.js
@@ -8,18 +8,18 @@ import store from './store'
 
 Amplify.configure({
   Auth: {
-    region: process.env.AWS_APPSYNC_REGION,
-    userPoolId: process.env.AWS_APPSYNC_USER_POOL_ID,
-    userPoolWebClientId: process.env.AWS_APPSYNC_USER_POOL_WEB_CLIENT_ID,
-    mandatorySignIn: process.env.AWS_APPSYNC_MANDATORY_SIGN_IN
+    region: process.env.VUE_APP_AWS_APPSYNC_REGION,
+    userPoolId: process.env.VUE_APP_AWS_APPSYNC_USER_POOL_ID,
+    userPoolWebClientId: process.env.VUE_APP_AWS_APPSYNC_USER_POOL_WEB_CLIENT_ID,
+    mandatorySignIn: process.env.VUE_APP_AWS_APPSYNC_MANDATORY_SIGN_IN
   }
 })
 
 
 const appConfig = {
-  'aws_appsync_graphqlEndpoint': process.env.AWS_APPSYNC_GRAPH_QL_ENDPOINT,
-  'aws_appsync_region': process.env.AWS_APPSYNC_REGION,
-  'aws_appsync_authenticationType': process.env.AWS_APPSYNC_AUTHENTICATION_TYPE
+  'aws_appsync_graphqlEndpoint': process.env.VUE_APP_AWS_APPSYNC_GRAPH_QL_ENDPOINT,
+  'aws_appsync_region': process.env.VUE_APP_AWS_APPSYNC_REGION,
+  'aws_appsync_authenticationType': process.env.VUE_APP_AWS_APPSYNC_AUTHENTICATION_TYPE
 }
 Amplify.configure(appConfig)
 
