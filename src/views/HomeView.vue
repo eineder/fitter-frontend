d3fb10c73e5e3ea1df0a07d943b3969297908d0d
max <max.eineder@googlemail.com>
2023-10-30 11:58:59 +0100

    Add components for home view with dummy contents

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div id="app" class="flex container h-screen w-full">
+    <!-- side navigation -->
+    <SideNav />
+
+    <!-- tweets -->
+    <TweetsView />
+
+    <!-- default right bar -->
+    <DefaultRightBar />
+
   </div>
 </template>
 
 <script>
+import SideNav from '../components/SideNav';
+import TweetsView from "../components/TweetsView";
+import DefaultRightBar from '../components/DefaultRightBar';
+
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    SideNav,
+    TweetsView,
+    DefaultRightBar
+  }
 }
 </script>
+
+<style>
+/* div {
+  border: 1px dashed blue;
+  padding: 15px;
+}
+
+div.items-center.justify-center {
+  background-color: rgba(240, 240, 240, 0.5);
+} */
+</style>
