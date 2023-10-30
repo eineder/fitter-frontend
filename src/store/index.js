8edd9b4b98c872ee19fa0fdc473b77eac1aa03b6
max <max.eineder@googlemail.com>
2023-10-30 12:53:30 +0100

    Add twitter vuex module

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import authentication from './modules/authentication';
 import signup from './modules/signup';
+import twitter from './modules/twitter';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
         authentication,
-        signup
+        signup,
+        twitter
     }
 })
---
+++ src/store/modules/twitter/actions.js
@@ -0,0 +1,9 @@
+import { getMyProfile } from '../../../lib/backend';
+
+
+export default {
+    async setProfile({ commit }) {
+        const profile = await getMyProfile();
+        commit('PROFILE_SET', profile);
+    }
+};
