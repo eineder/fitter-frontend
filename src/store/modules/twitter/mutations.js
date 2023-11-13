95dd78fb803c7012975417f4760fcd081af20c63
max <max.eineder@googlemail.com>
2023-11-13 22:00:31 +0100

    Correct tweets updating.

--- src/store/modules/twitter/mutations.js
+++ src/store/modules/twitter/mutations.js
@@ -7,7 +7,7 @@ export default {
         state.tweets = timeline;
     },
     TWITTER_CREATE(state, newTweet) {
-        const tweets = [...state.tweets.tweets];
-        state.tweets.tweets = tweets.unshift(newTweet);
-    }
+    const tweets = [newTweet, ...state.tweets.tweets];
+    state.tweets.tweets = tweets;
+  },
 };
